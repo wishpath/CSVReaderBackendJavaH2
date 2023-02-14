cc05a08d663ea912765334bfb26efaaa7d9d1d26
Simas <simonas.aruna@gmail.com>
2023-02-14 09:38:58 +0200

    Changes for file uploading in the front

--- src/main/java/b3_web_csv_uploader/controller/FileController.java
+++ src/main/java/b3_web_csv_uploader/controller/FileController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -27,6 +28,7 @@ import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("files")
+@CrossOrigin("*")
 public class FileController {
 
     private static final Logger logger = LoggerFactory.getLogger(FileController.class);
